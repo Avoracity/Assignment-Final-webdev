6504083e73e62b743012ca7b71fb774643715c51
Michael Alvarez <michael.alvarez687@myhunter.cuny.edu>
2021-07-10 23:10:41 -0400

    email img gpa added
    methods for students to insert

--- starter-server-main/database/models/Student.js
+++ starter-server-main/database/models/Student.js
@@ -11,7 +11,30 @@ const Student = db.define("student", {
   lastname: {
     type: Sequelize.STRING,
     allowNull: false
-  }
+  },
+
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    defaultValue: "N/A",
+    isEmail: true,
+  },
+
+  imgurl: {
+    type: Sequelize.STRING,
+    defaultValue:
+      "https://discordia.me/uploads/easter-eggs/banana.png",
+    validate: {
+      isUrl: true,
+    },
+  },
+
+  gpa: {
+    type: Sequelize.STRING,
+    defaultValue: "N/A",
+    isDecimal: true,
+    validate: {min: 0.0, max: 4.0 },
+  },
 
 });
 
